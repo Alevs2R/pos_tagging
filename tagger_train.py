443a6fc8f16bba6edc6e15d42fe1bfe54f2ecb8c
Алексей Евлампьев <a.evlampev@innopolis.ru>
2020-02-18 00:10:49 +0300

    add words_tags_split

--- tagger_train.py
+++ tagger_train.py
@@ -10,12 +10,27 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-
 def train_model(train_file, model_file):
-    # write your code here. You can add functions as well.
-    # use torch library to save model parameters, hyperparameters, etc. to model_file
-    
+    words, tags = words_tags_split(train_file)
     print('Finished...')
+
+
+def words_tags_split(train_file):
+    reader = open(train_file)
+    train_lines = reader.readlines()
+    reader.close()
+
+    words = []
+    tags = []
+
+    for train_line in train_lines:
+        cur_train_pairs = train_line.strip().split(' ')
+        for pair in cur_train_pairs:
+            splitted = pair.split('/')
+            words.append(splitted[0])
+            tags.append(splitted[1])    
+
+    return words, tags        
 
 
 if __name__ == "__main__":
